3f2930e1ec7f383f9d24ca1f457cd63570ec6c19
henrique almeida <frahcoutinho@hotmail.com.br>
2021-04-24 15:41:21 -0300

    first commit

---
+++ Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documetario = 4,
+        Drama=5,
+        Faroeste = 6,
+        Fantasia = 7,
+        Espionagem = 8,
+        Ficcao_cientifca = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13
+    }
+    
+}
